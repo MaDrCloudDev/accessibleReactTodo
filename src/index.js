0fc4d64caf201f2b9bb63df84220e5c386d5a88c
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2022-08-10 21:50:08 -0700

    clean up
    clean up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 const DATA = [
-    { id: "todo-0", name: "Wake up", completed: true },
-    { id: "todo-1", name: "Eat Lunch", completed: false },
-    { id: "todo-2", name: "Sleep", completed: false }
-  ];
+  { id: "todo-0", name: "Wake up", completed: true },
+  { id: "todo-1", name: "Eat Lunch", completed: false },
+  { id: "todo-2", name: "Sleep", completed: false },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-  <App tasks={DATA} />, document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App tasks={DATA} />, document.getElementById("root"));
